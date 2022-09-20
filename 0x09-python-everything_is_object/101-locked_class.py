324cb2297eb997887c442f8ac49faf04c0453e62
Stella-Wanjiru <stellahallexies@gmail.com>
2022-09-20 12:02:03 -0700

    Answers

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,11 +3,11 @@
 """
 
 class LockedClass:
-        """A locked class that only lets the user dynamically create the instance attribute 'first_name'
-            """
+    """A locked class that only lets the user dynamically create the instance attribute 'first_name'
+    """
 
-             __slots__ = ['first_name']
+    __slots__ = ['first_name']
 
-             def __init__(self):
-                 """ Init method """
-                 pass
+    def __init__(self):
+    """ Init method """
+    pass
